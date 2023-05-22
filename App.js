59a3298f088234d57c02f025f7b18549c15e32dd
Sami Hafid <sahafid@e2r4p15.1337.ma>
2023-05-22 20:24:55 +0100

    .

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MainScreen from './Screens/MainScreen';
 
+
+
 export default function App() {
   return (
     <View>
